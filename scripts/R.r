b105145b7c0dea5356e06fac884a21ff3766a8ef
Elcio Cezario <ecsanchesjr@hotmail.com>
2017-10-18 22:11:51 -0200

    Last version scripts

--- scripts/R.r
+++ scripts/R.r
@@ -25,8 +25,12 @@ for (i in 1:10){
 #dados <- read.csv(file="/home/elss/Documentos/Git Repos/project-ia/logs/berlin52/test-10-0-0-10-2/run_1.stats.csv", header=TRUE, sep=",")
 data <- data.frame(max[,1], max[,3])
 plot(data, type="l", col=1, xlab="Gerações", ylab="Distância do melhor indivíduo", main="BATATA", lwd="1")
+abline(h=11000, col="Black", lwd=2, lty=10)
+#text(12000, , "text", srt=0.2, col = "blue")
+text(-1000000, 12000, "cutoff", col = "red") 
 cat ("uahuahauhaa")
-
+tab <- data.frame(MaxValues=max(dados[,3]), MinValues=min(dados[,3]))
+axis(1, at = 11000)
 for(i in 1:10){
   fileT <- paste("/home/elss/Documentos/Git Repos/project-ia/logs/berlin52/test-10-0-0-10-2/run_",i,".stats.csv", sep="")
   dados <- read.csv(file=fileT, header=TRUE, sep=",")
@@ -34,7 +38,50 @@ for(i in 1:10){
   cat("\n")
   cat("==========================================================")
   cat("\n")
-  cat(dados[,3])
+  #cat(dados[,3])
   lines(data, type="l", col=i, lwd="1")
+  
+  tabAux <- data.frame(MaxValues=max(dados[,3]), MinValues=min(dados[,3]))
+  
+  tab <- rbind(tab, tabAux)
 }
+
+print(xtable(tab),hline.after=c(2,3)) 
+
+pdf("table.pdf", height=11, width=11)
+print(tab)
+
 legend("topright", legend=c("10 Mut 10 Elit", "2"), col=c(1,2), lty=1.2, cex=2.8)
+
+
+library(xtable)
+tab1 <-data.frame(x = max(dados[,3]),y = min(dados[,3]), stringsAsFactors = FALSE)
+tab1$x[0] <- max(dados[,3])
+tab1$y[0] <- max(dados[,3])
+colors <- c("black", "blue", "blueviolet", "brown1", "darkgoldenrod", "chocolate", "darkgreen", "deeppink", "green", "olivedrab", "orangered", "yellowgreen")
+
+
+#################
+
+
+for (i in 1:12){
+  dados <- read.csv(file="/home/elss/Documentos/Git Repos/project-ia/logs/berlin52/test-10-0-0-10-2/run_1.stats.csv", header=TRUE, sep=",")
+  if(i==1){
+    max = dados
+    minY = min(dados[,3])
+    maxY = max(dados[,3])
+  }
+  if(length(max[,1]) < length(dados[,1])){
+    max = dados
+  }
+  if(min(dados[,3]) < minY){
+    minY = min(dados[,3])
+  }
+  if(max(dados[,3]) < minY){
+    maxY = min(dados[,3])
+  }
+}
+
+
+data <- data.frame(max[,1], max[,3])
+plot(data, type="n", xlab="Generations", ylab="Distance of Best Tour", lwd="1", main=args[26], ylim=c(minY, maxY))
--- scripts/graphicsGeneration.r
+++ scripts/graphicsGeneration.r
@@ -1,10 +1,11 @@
 #!/usr/bin/Rscript
 args = commandArgs(trailingOnly=TRUE)
+library(xtable)
 
+colors <- c("darkolivegreen", "blue", "blueviolet", "brown1", "darkgoldenrod", "chocolate", "darkgreen", "deeppink", "green", "olivedrab", "orangered", "black", "black")
 
 fileName <- paste("../logs/Graphs/", args[25], ".pdf", sep="")
 pdf(fileName, height=10, width=10)
-
 
 for (i in 1:12){
   dados <- read.csv(file=args[i], header=TRUE, sep=",")
@@ -23,19 +24,35 @@ for (i in 1:12){
     maxY = min(dados[,3])
   }
 }
-data <- data.frame(max[,1], max[,3])
-plot(data, type="l", col=1, xlab="Generations", ylab="Distance of Best Tour", lwd="1", main=args[26], ylim=c(minY, maxY))
 
-library(RColorBrewer)  # só 9 colors
-cols <- brewer.pal(n=12,name="Set1")
+if(args[27] == "berlin52"){
+  opt=7542
+}else{
+  opt=14379
+
+}
+
+data <- data.frame(max[,1], max[,3])
+minY=(opt-1000)
+plot(data, type="l", col="white", xlab="Generations", ylab="Distance of Best Tour", lwd="1", main=args[26], ylim=c(minY, maxY))
+
+tab <- data.frame(MaxValues=max(dados[,3]), MinValues=min(dados[,3]))
 
 for(i in 1:12){
   dados <- read.csv(file=args[i], header=TRUE, sep=",")
   data <- data.frame(dados[,1], dados[,3])
-  lines(data, type="l", col=cols[i-1], lwd="1.5")
+  lines(data, type="l", col=colors[i-1], lwd="1.5")
+
+  tabAux <- data.frame(MaxValues=max(dados[,3]), MinValues=min(dados[,3]))
+  tab <- rbind(tab, tabAux)
 }
+
+abline(h=opt, col="Black", lwd=2, lty=10)
+
+write(fileName, "/home/elss/Documentos/Git Repos/project-ia/logs/tables.txt", sep="\t", append=TRUE)
+write.table(print(xtable(tab),hline.after=c(2,3)) , "/home/elss/Documentos/Git Repos/project-ia/logs/tables.txt", sep="\t", append=TRUE)
 
 grid(NULL, NULL, lty=10)
 
-legend("topright", legend=c(args[13],args[14],args[15],args[16],args[17],args[18],args[19],args[20],args[21],args[22],args[23],args[24]), col=c(1,2,3,4,5,6,7,8,9,10,11,12), lty=1, cex=2, lwd=3, title="Elitism % - Mutation %", bg="white")
+legend("topright", legend=c(args[13],args[14],args[15],args[16],args[17],args[18],args[19],args[20],args[21],args[22],args[23],args[24]), col=c(colors[0], colors[1], colors[2], colors[3], colors[4], colors[5], colors[6], colors[7], colors[8], colors[9], colors[10], colors[11]), lty=1, cex=0.8, lwd=3, title="Elitism % - Mutation %", bg="white")
 dev.off()
